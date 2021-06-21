d7511db3f0a097289fb4ce394d197020b907a4ea
flaviosoliver <flavsoliver@gmail.com>
2021-06-21 17:10:40 -0300

    page register

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,9 +1,11 @@
 import Login from './Login';
 import Main from './Main';
 import Home from './Home';
+import Register from './Register';
 
 export {
   Login,
   Main,
   Home,
+  Register,
 };
--- front-end/src/services/routes.jsx
+++ front-end/src/services/routes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Main, Login, Home } from '../pages';
+import { Main, Login, Home, Register } from '../pages';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Main} />
     <Route exact path="/login" component={Login} />
+    <Route exact path="/register" component={Register} />
     <Route exact path="/home" component={Home} />
   </Switch>
 );
